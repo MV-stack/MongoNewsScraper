c2f3713c12da35e645ad1d828eb76007713b5c66
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-06-10 09:33:21 -0700

    created server.js file, added views/layouts folder with handlebars, routes folder and apiroutes, public folder and app.js and index.html, models and js files

---
+++ routes/apiroutes.js
@@ -0,0 +1,99 @@
+var db = require ("../models")
+var axios = require("axios");
+var cheerio = require("cheerio");
+function apiroutes(app){ 
+  // A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("https://apnews.com").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+  
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("div.FeedCard").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+  
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("div.CardHeadline")
+          .children("a")
+          .children("h1")
+          .text();
+        result.link = $(this)
+        .children("div.CardHeadline")
+          .children("a")
+          .attr("href");
+          result.summary = $(this)
+          .children("a")
+          .children("div.content")
+          .children("p")
+          .text()
+          console.log(result)
+        //  https://storage.googleapis.com/afs-prod/media/03c8f4a13e37403992d670c34290530b/800.jpeg
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            //console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+          });
+      });
+  
+      // Send a message to the client
+      res.send("Scrape Complete");
+    });
+  });
+  
+  // Route for getting all Articles from the db
+  app.get("/articles", function(req, res) {
+    // TODO: Finish the route so it grabs all of the articles
+    db.Article.find({}).then(function(dbArticle){
+      res.json(dbArticle)
+    }).catch(function(err){
+      res.json(err)
+    })
+  });
+  
+  // Route for grabbing a specific Article by id, populate it with it's note
+  app.get("/articles/:id", function(req, res) {
+    // TODO
+   db.Article.findOne({}).populate("note").then(function(dbArticle){
+     res.json(dbArticle)
+   }).catch(function(err){
+     res.json(err)
+   })
+    // Finish the route so it finds one article using the req.params.id,
+    // and run the populate method with "note",
+    // then responds with the article with the note included
+  });
+  
+  // Route for saving/updating an Article's associated Note
+  app.post("/articles/:id", function(req, res) {
+    // TODO
+    db.Article.create(req.body)
+    .then(function(dbArticle) {
+      // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.User.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If the User was updated successfully, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
+    // save the new note that gets posted to the Notes collection
+    // then find an article from the req.params.id
+    // and update it's "note" property with the _id of the new note
+  });
+  
+}
+
+module.exports = apiroutes
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+
+
+ 
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+var apiroutes = require ("./routes/apiroutes")
+apiroutes(app)
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/apnews", { useNewUrlParser: true });
+
+// Routes
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
